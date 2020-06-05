d914c565a5d73bc44797cf609139ff245f845b27
Alastair Dawson <alastair.j.dawson@gmail.com>
2020-06-05 16:05:50 -0700

    update edit display name

--- src/components/NpiRegistry/NpiRegistry.form.ts
+++ src/components/NpiRegistry/NpiRegistry.form.ts
@@ -1,10 +1,10 @@
 import nestedComponentForm from 'formiojs/components/_classes/nested/NestedComponent.form';
-import CheckMatrixEditDisplay from './editForm/NpiRegistry.edit.display';
+import NpiRegistryEditDisplay from './editForm/NpiRegistry.edit.display';
 export default function(...extend) {
   return nestedComponentForm([
     {
       key: 'display',
-      components: CheckMatrixEditDisplay
+      components: NpiRegistryEditDisplay
     }
   ], ...extend);
 }
